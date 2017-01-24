955436b8d3f345401ff98cd3e9218cb215410257
Chengcheng <lcc199533@gmail.com>
2017-01-24 21:44:03 +0800

    T2A3 Exercise 1

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -219,7 +219,18 @@ public class AddressBook {
 
     public static void main(String[] args) {
         showWelcomeMessage();
-        processProgramArgs(args);
+        if (args.length >= 2) {
+            showToUser(new String[] {MESSAGE_INVALID_PROGRAM_ARGS});
+            exitProgram();
+        }
+
+        if (args.length == 1) {
+            setupGivenFileForStorage(args[0]);
+        }
+
+        if(args.length == 0) {
+            setupDefaultFileForStorage();
+        }
         loadDataFromStorage();
         while (true) {
             String userCommand = getUserInput();
@@ -370,7 +381,7 @@ public class AddressBook {
      */
 
     /**
-     * Executes the command as specified by the {@code userInputString}
+     * Executes the command as specified by the {@code userInputString}.
      *
      * @param userInputString  raw input from user
      * @return  feedback about how the command was executed
